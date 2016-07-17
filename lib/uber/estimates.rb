b8cf32325434873e8ed86e8eeb53d427e687bc87
Bogdan <bogdan.mihai.timofte@gmail.com>
2016-07-17 22:58:24 +0300

    Added configuration for server token

--- lib/uber/estimates.rb
+++ lib/uber/estimates.rb
@@ -1,7 +1,24 @@
+require 'pry'
 require "uber/estimates/version"
+require "uber/estimates/configuration"
 
 module Uber
   module Estimates
-    # Your code goes here...
+    class << self
+      attr_accessor :configuration
+    end
+
+    def self.configuration
+      @configuration ||= Configuration.new
+    end
+
+    def self.configure
+      yield(configuration)
+    end
+
+    def self.get_estimations(type, params)
+      binding.pry
+    end
+
   end
 end
